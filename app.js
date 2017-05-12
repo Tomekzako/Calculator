5ce47ed1760e150eaf83c94a7335cd071d7f6f69
Tomasz Chyc <tomek.chyc55@gmail.com>
2017-05-12 18:21:14 +0200

    Basic functionality of the calculator

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const keys = document.querySelectorAll('.keys__one');    
-    
-    for ( var i=0; i<keys.length; i++){
-        keys[i].addEventListener('click', function(){
+    const keys = document.querySelectorAll('.keys__one');
+
+    for (var i = 0; i < keys.length; i++) {
+        keys[i].addEventListener('click', function () {
             let input = document.querySelector('.keys__one__result');
             let inputVal = input.innerHTML;
             let newVal = this.innerHTML;
-            
+
             if (newVal == 'C') {
-                inputVal = '';
+                input.innerHTML = '';
+            }
+
+            if (newVal == '=') {
+                console.log('gusia');
+                var equation = inputVal;
+                equation = equation.replace(/&#215/g, '*').replace(/&#247/g, '/');
+
+                if (equation) {
+                    input.innerHTML = eval(equation);
+                }
             } else {
-            input.innerHTML += newVal;
-            console.log(inputVal);
+                input.innerHTML += newVal;
             }
         });
     }
