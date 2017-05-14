a6475f68a322f589ab07e15b5544cb37ef2fc55d
Tomasz Chyc <tomek.chyc55@gmail.com>
2017-05-14 20:55:59 +0200

    Add operator only if last element is not operator

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const keys = document.querySelectorAll('.keys__one');
     const operators = ['+', '-', 'x', '÷'];
-    
+
 
     for (var i = 0; i < keys.length; i++) {
         keys[i].addEventListener('click', function () {
@@ -12,26 +12,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if (newVal == 'C') {
                 input.innerHTML = '';
-            }
-
-            else if (newVal == '=') {
+            
+            } else if (newVal == '=') {
                 var equation = inputVal;
                 equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
                 console.log(equation);
                 if (equation) {
                     input.innerHTML = eval(equation);
                 }
-                
-                
-            }
-            
-            else if(operators.indexOf(newVal) > -1){
-                if(inputVal != ''){
+
+            } else if (operators.indexOf(newVal) > -1) {
+                let lastEl = inputVal[inputVal.length - 1];
+
+                if (inputVal != '' && operators.indexOf(lastEl) == -1) {
                     input.innerHTML += newVal;
                 }
-            }
-            
-            else {
+            } else {
                 input.innerHTML += newVal;
             }
         });
