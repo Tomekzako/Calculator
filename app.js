cb6ffeb86c5c7520a0b0b2b701130815b49064d4
Tomasz Chyc <tomek.chyc55@gmail.com>
2017-05-15 14:25:12 +0200

    Max 1 decimal in a number

--- app.js
+++ app.js
@@ -2,30 +2,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const keys = document.querySelectorAll('.keys__one');
     const operators = ['+', '-', 'x', '÷'];
-
+    let decimal = false;
 
     for (var i = 0; i < keys.length; i++) {
-        keys[i].addEventListener('click', function () {
+        keys[i].addEventListener('click', function (e) {
+            e.preventDefault();
             let input = document.querySelector('.keys__one__result');
             let inputVal = input.innerHTML;
             let newVal = this.innerHTML;
 
             if (newVal == 'C') {
                 input.innerHTML = '';
+                decimal = false;
 
             } else if (newVal == '=') {
                 let equation = inputVal;
-                let lastEl = equation[equation.length-1];
+                let lastEl = equation[equation.length - 1];
                 equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
                 console.log(equation);
-                
-                if(operators.indexOf(lastEl) > -1 || lastEl == '.'){
-                    equation = equation.replace(/.$/,'');
+
+                if (operators.indexOf(lastEl) > -1 || lastEl == '.') {
+                    equation = equation.replace(/.$/, '');
                 }
-                
+
                 if (equation) {
                     input.innerHTML = eval(equation);
                 }
+                decimal = false;
 
             } else if (operators.indexOf(newVal) > -1) {
                 let lastEl = inputVal[inputVal.length - 1];
@@ -38,6 +41,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 if (operators.indexOf(lastEl) > -1 && inputVal.length > 1) {
                     input.innerHTML = inputVal.replace(/.$/, newVal);
+                }
+                decimal = false;
+            } else if (newVal == '.') {
+                if (!decimal) {
+                    input.innerHTML += newVal;
+                    decimal = true;
                 }
             } else {
                 input.innerHTML += newVal;
